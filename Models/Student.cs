6f00d9ee1b976693faa700a14f5ece6171772c99
Tran Van Hung <hungtranvan170.com>
2024-12-10 15:33:04 +0700

    Add: Student, Course, Enrollment, SchoolContext, DbInitilizer

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models{
+    public class Student{
+        public int ID {get; set;}
+
+        public string LastName {get; set;} = null!;
+
+        public string FirstMidName {get; set;} = null!;
+
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate {get; set;}
+
+        public ICollection<Enrollment> Enrollments {get; set;} = null!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+var app = builder.Build();
+using(var scope = app.Services.CreateScope()){
+    var serviceProvider = scope.ServiceProvider;
+    try{
+        DbInitializer.Initialize(serviceProvider);
+    }catch(Exception ex){
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+        
+    }
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
